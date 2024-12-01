16c93b147336eb1219b9956f599757d3773a407e
Azziz Abdul Ghofur <152408235+Azzizag@users.noreply.github.com>
2024-12-01 20:50:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,125 @@
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    ListKaryawan kywn;
+    adrKaryawan adrkar;
+    infotypeKaryawan infkar;
+    ListProjek prjk;
+    adrProjek adrprjk;
+    infotypeProjek infprjk;
+    ListRelasi rls;
+    int N;
+
+    int karyawanOption, projekOption, option;
+    createListKaryawan(kywn);
+    createListProjek(prjk);
+    createListRelasi(rls);
+
+
+    TampilanMainMenu();
+    cin >> option;
+    while (option < 7){
+        switch(option){
+        case 1:
+            TampilanMenukaryawan();
+            cin >> karyawanOption;
+            while (karyawanOption < 4){
+            switch(karyawanOption){
+            case 1:
+                cout <<"Masukan berapa banyak data karyawan yang ingin di input: ";
+                cin >> N;
+                for(int i = 0; i < N; i++){
+                        cout <<"===================="<<endl;
+                        cout << "ID: ";
+                        cin >> infkar.id_karyawan;
+                        cout << "Nama: ";
+                        cin >> infkar.nama_karyawan;
+                        cout << "Alamat: ";
+                        cin >> infkar.alamat_karyawan;
+                        cout << "Kontak: ";
+                        cin >> infkar.telp_karyawan;
+                        cout << "Email: ";
+                        cin >> infkar.email_karyawan ;
+                        cout << "eror1" << endl;
+                        adrkar = createElemenKaryawan(infkar);
+                        cout << "eror2" << endl;
+                    insertLastKaryawan(kywn, adrkar);
+                    cout << "eror3" << endl;
+                }
+
+
+                case 2:
+//            searchDataKaryawan();
+
+            case 3:
+//            deleteFirstKaryawan();
+
+            default:
+                break;
+            }
+            TampilanMenukaryawan();
+            cin >> karyawanOption;
+
+            }
+
+
+        case 2:
+            TampilanMenuproyek();
+            cin >> projekOption;
+            while (projekOption < 4){
+            switch(projekOption){
+            case 1:
+                cout <<"Masukan berapa banyak data projek yang ingin di input: ";
+                cin >> N;
+                for(int i = 0; i < N; i++){
+                    cout <<"===================="<<endl;
+                    cout << "ID: ";
+                    cin >> infprjk.id_projek;
+                    cout << "Nama: ";
+                    cin >> infprjk.nama_projek ;
+                    cout << "Jenis: ";
+                    cin >> infprjk.jenis_projek ;
+                    cout << "Durasi: ";
+                    cin >> infprjk.durasi_projek;
+                    cout << "Anggaran: ";
+                    cin >> infprjk.anggaran_projek;
+                    adrprjk = createElemenProjek(infprjk);
+                    insertFirstProjek(prjk, adrprjk);
+
+                }
+
+
+            case 2:
+//            searchDataProjek();
+
+            case 3:
+//            deleteLastProjek();
+
+            default:
+                break;
+            }
+            TampilanMenuproyek();
+            cin >> projekOption;
+        }
+
+
+    case 3:
+//        showAllDataWithRelasi();
+
+    case 4:
+        showDataKaryawan(kywn);
+
+    case 5:
+        showDataProjek(prjk);
+
+    case 6:
+//        countDataProjekFromKaryawan();
+
+    default:
+        break;
+    }
+        TampilanMainMenu();
+        cin >> option;
+    }
+
 }
